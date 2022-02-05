7f6519cc4382d99538c89d8cf4ea4462fa3b3013
0dminnimda <0dminnimda@gmail.com>
2022-02-05 13:42:23 +0300

    Implement 04\fill\Fill + my experiment

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -11,4 +11,82 @@
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+
+// press once - black
+// press twise - white
+// press third time - black
+// etc ..
+//
+//    @COLOR
+//    M=0
+//
+//(WAIT)
+//    @KBD
+//    D=M
+//    @WAIT
+//    D;JEQ
+//
+//    @COLOR
+//    M=M+1
+//    D=M
+//    @SET-1
+//    D;JGT
+//
+// (SET0)
+//    @FILL
+//    0;JMP
+//
+//(SET-1)
+//    @COLOR
+//    M=-1
+
+
+
+// press - black
+// unpress - white
+////////////
+(WAIT)
+    @KBD
+    D=M
+    @FILL-WHITE
+    D;JEQ
+
+// (FILL-BLACK)
+    @COLOR
+    M=-1
+    @FILL
+    0;JMP
+
+(FILL-WHITE)
+    @COLOR
+    M=0
+    @FILL
+    0;JMP
+
+
+////////////
+(FILL)
+    @SCREEN
+    D=A
+    @POINTER
+    M=D
+
+(LOOP)
+    @POINTER
+    D=M
+    @KBD
+    D=A-D
+    @WAIT
+    D;JEQ
+
+    @COLOR
+    D=M
+    @POINTER
+    A=M
+    M=D
+
+    @POINTER
+    M=M+1
+
+    @LOOP
+    0;JMP
